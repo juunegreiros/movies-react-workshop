6a93d38785a52037f6b4bbb5efa5edd67c7bfc0f
Ju <juliana.negreiros@codeminer42.com>
2018-10-01 00:42:34 -0300

    Add all services

--- public/App.js
+++ public/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from './Router.jsx';
+import Router from './Router';
 
 import './styles/index.scss';
 
---
+++ public/services/movies/searchMovies.jsx
@@ -0,0 +1,9 @@
+import movieDB, { key } from '../api.jsx';
+
+export default query => {
+  movieDB
+    .get(`search/movie?api_key=${key}&language=pt-BR&query=${query}`)
+    .then(response => response.data)
+      .then(data => console.log(data))
+    .catch(error => console.log(error));
+};
